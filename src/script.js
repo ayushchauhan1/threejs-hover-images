f6dc01ccb81cfe47b2ae0d942dd0187c4ab86db8
ayushchauhan1 <ayush_c@ar.iitr.ac.in>
2021-07-04 01:25:09 +0530

    complete

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import gsap from 'gsap' // its an js library for creating 
+import gsapCore from 'gsap/gsap-core'
 
 
 // texture loader
@@ -22,11 +24,19 @@ for(let i=0; i<=4;i++){
     })
 
     const img = new THREE.Mesh(geometry, material)
-    img.position.set(1,i*-2.2)
+    img.position.set(0.8,i*-2.2)
 
     scene.add(img)
 
 }
+
+let obj =[]
+scene.traverse((Object)=>
+{
+    if(Object.isMesh){
+        obj.push(Object)
+    }
+})
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
@@ -83,9 +93,27 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+//mouse
+
+window.addEventListener("wheel",onmousewheel)
+let y=0
+let position=0
+
+function onmousewheel(event)
+{
+   y=event.deltaY*0.0007
+}
+
+const mouse = new THREE.Vector2() // vector2- it will take x, y value
+
+window.addEventListener('mousemove', (event)=>{
+    mouse.x = event.clientX/ sizes.width*2 -1
+    mouse.y = -(event.clientY/ sizes.height)*2 +1
+})
 /**
  * Animate
  */
+ const raycaster = new THREE.Raycaster(); // use for any type of  interacting things like hover
 
 const clock = new THREE.Clock()
 
@@ -93,13 +121,38 @@ const tick = () =>
 {
 
     const elapsedTime = clock.getElapsedTime()
+    // raycaster
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(obj)
+
+    for (const intersect of intersects){
+       gsap.to(intersect.object.scale, {x: 1.5 , y: 1.5})
+       gsap.to(intersect.object.rotation, { y: -0.1})
+       gsap.to(intersect.object.position, {z: 0.2})
+
+    }
+
+    for(const object of obj)
+    {
+        if(!intersects.find(intersect => intersect.object == object)){
+            gsap.to(object.scale, {x: 1 , y: 1})
+            gsap.to(object.rotation, { y: 0})
+            gsap.to(object.position, {z: 0}) 
+        }
+    }
 
     // Update objects
     // sphere.rotation.y = .5 * elapsedTime
 
-    // Update Orbital Controls
-    // controls.update()
 
+    position+= y
+    y*=0.9
+
+
+     camera.position.y = -position
+
+      // Update Orbital Controls
+    // controls.update()
     // Render
     renderer.render(scene, camera)
 
